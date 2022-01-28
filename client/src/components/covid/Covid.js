5ca20a8d1fc7ae9889829778822f7e0d0e62c9e6
tungtt30 <tungchipsctn2@gmail.com>
2022-01-28 14:07:59 +0700

    kkkk

--- client/src/components/covid/Covid.js
+++ client/src/components/covid/Covid.js
@@ -4,15 +4,19 @@ import Table from 'react-bootstrap/Table'
 const Covid = () => {
     const aoi = 'https://static.pipezero.com/covid/data.json'
     const [data, setData] = useState([])
+    const [today, setToday] = useState({ internal: { cases: 0, death: 0, recovered: 0, } })
+    const [total, setTotal] = useState({ cases: 0, death: 0, recovered: 0 })
 
     useEffect(() => {
 
         fetch(aoi)
             .then(res => res.json())
             .then(res => {
+                console.log(res)
                 setData(res.locations)
+                setToday(res.today)
+                setTotal(res.total.internal)
             })
-
     }, [])
 
 
@@ -22,6 +26,13 @@ const Covid = () => {
         <>
             <div style={{ margin: '120px auto', width: '95vw' }}>
                 <h2>Tình hình Covid-19 Việt Nam</h2>
+                <div>
+                    <ul>
+                        <li>Ca mắc mới hôm nay: {today.internal.cases} ca</li>
+                        <li>Ca tử vong: {today.internal.death} ca</li>
+                        <li>Khỏi bệnh: {today.internal.recovered} ca</li>
+                    </ul>
+                </div>
                 <Table striped bordered hover >
                     <thead>
                         <tr>
@@ -44,6 +55,13 @@ const Covid = () => {
                                 </tr>
                             )
                         })}
+                        <tr>
+                            <td>#</td>
+                            <td>Cả nước</td>
+                            <td>{total.cases}</td>
+                            <td>{today.internal.cases}</td>
+                            <td>{total.death}</td>
+                        </tr>
                     </tbody>
                 </Table>
             </div>
