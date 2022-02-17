0abae979035c459588ad98438907df730bd669f5
tungtt30 <tungchipsctn2@gmail.com>
2022-02-17 15:00:37 +0700

    fix nir title

--- client/src/components/nir/Nir.jsx
+++ client/src/components/nir/Nir.jsx
@@ -1,65 +1,66 @@
-import React, { useContext, memo } from 'react'
-import Card from 'react-bootstrap/Card'
-import Col from "react-bootstrap/Col"
-import { NirContext } from "./NirLayout"
-import Spinner from 'react-bootstrap/esm/Spinner'
+import React, { useContext, memo } from "react";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import { NirContext } from "./NirLayout";
+import Spinner from "react-bootstrap/esm/Spinner";
 
 const Nir = (props) => {
+  const { setSongIndex, setCurrentSong, playSong, currentSong, isPlaying } =
+    useContext(NirContext);
 
-    const { setSongIndex, setCurrentSong, playSong, currentSong, isPlaying } = useContext(NirContext)
+  let btnSpinner = null;
+  let btnText = "Play";
 
-    let btnSpinner = null
-    let btnText = 'Play'
+  const handleClick = async () => {
+    await setCurrentSong(props);
+    await setSongIndex(props.index);
+    await playSong();
+  };
 
+  if (currentSong.url === props.url && isPlaying) {
+    btnSpinner = (
+      <Spinner
+        as="span"
+        animation="grow"
+        size="sm"
+        role="status"
+        aria-hidden="true"
+      />
+    );
+    btnText = " Playing ";
+  } else if (currentSong.url === props.url && !isPlaying) {
+    btnSpinner = (
+      <Spinner
+        as="span"
+        animation="border"
+        size="sm"
+        role="status"
+        aria-hidden="true"
+      />
+    );
+    btnText = "";
+  }
+  
 
+  return (
+    <>
+      <Col>
+        <Card
+          className="mb-5 cardClass"
+          style={{ cursor: "pointer" }}
+          onClick={handleClick}
+        >
+          <Card.Img variant="top" className="card-image" src={props.image} />
+          <Card.Body>
+            <Card.Title>
+              {props.name} <span style={{ color: "red" }}>{btnSpinner}</span>
+            </Card.Title>
+            <Card.Text>{props.singer}</Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+    </>
+  );
+};
 
-
-    const handleClick = async () => {
-        await setCurrentSong(props)
-        await setSongIndex(props.index)
-        await playSong()
-    }
-
-
-    if (currentSong.url === props.url && isPlaying) {
-        btnSpinner = (
-            <Spinner
-                as="span"
-                animation="grow"
-                size="sm"
-                role="status"
-                aria-hidden="true"
-            />
-        )
-        btnText = ' Playing '
-    } else if (currentSong.url === props.url && !isPlaying) {
-        btnSpinner = (
-            <Spinner
-                as="span"
-                animation="border"
-                size="sm"
-                role="status"
-                aria-hidden="true"
-            />
-        )
-        btnText = ''
-    }
-
-
-    return (
-        <>
-            <Col>
-                <Card className='mb-5 cardClass' style={{ cursor: 'pointer' }} onClick={handleClick}>
-                    <Card.Img variant='top' className='card-image' src={props.image} />
-                    <Card.Body>
-                        <Card.Title>{props.name} <span style={{ color: 'red' }}>{btnSpinner}</span></Card.Title>
-                        <Card.Text>{props.singer}</Card.Text>
-                    </Card.Body>
-                </Card>
-            </Col>
-
-        </>
-    )
-}
-
-export default memo(Nir) 
+export default memo(Nir);
